23c73e1309937a9985bef8287a6fc841661d0794
Marius Bock <marius.bock@gmail.com>
2025-07-18 15:27:51 +0200

    Added chapter 14

---
+++ ch14_generics/impl1.rs
@@ -0,0 +1,12 @@
+#![allow(unused)]
+fn main() {
+struct S; // Concrete type `S`
+struct GenericVal<T>(T); // Generic type `GenericVal`
+
+// impl of GenericVal where we explicitly specify type parameters:
+impl GenericVal<f32> {} // Specify `f32`
+impl GenericVal<S> {} // Specify `S` as defined above
+
+// `<T>` Must precede the type to remain generic
+impl<T> GenericVal<T> {}
+}
